73442542dc3ca073339e067029fb1a26870da7f0
sungyoungk <skim8700@gmail.com>
2022-06-25 19:38:52 +0900

    post CRUD 추가

--- hack/src/main/java/com/project/hack/dto/response/PostResponseDto.java
+++ hack/src/main/java/com/project/hack/dto/response/PostResponseDto.java
@@ -1,2 +1,61 @@
-package com.project.hack.dto.response;public class PostResponseDto {
+package com.project.hack.dto.response;
+
+import com.project.hack.model.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostResponseDto {
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+
+    private Long postId;
+
+    private String title;
+
+
+    private String content;
+
+
+    private String name;
+
+
+    private String email;
+
+
+    private String profile_image;
+
+
+    private String like;
+
+
+    private String category;
+
+    public PostResponseDto(Post post) {
+        this.postId = post.getPostId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.name = post.getName();
+        this.email = post.getEmail();
+        this.profile_image = post.getProfile_image();
+        this.like = post.getLike();
+        this.category = post.getCategory();
+        this.modifiedAt = post.getModifiedAt();
+        this.createdAt = post.getCreatedAt();
+    }
+
+    //”modifiedAt” :
+    //”createdAt”:
+
+
+
 }
--- hack/src/main/java/com/project/hack/model/Post.java
+++ hack/src/main/java/com/project/hack/model/Post.java
@@ -1,2 +1,85 @@
-package com.project.hack.model;public class Post {
+package com.project.hack.model;
+
+import com.project.hack.dto.response.PostResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post extends Timestamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String profile_image;
+
+    @Column(nullable = false)
+    private String like;
+
+    @Column(nullable = false)
+    private String category;
+
+
+
+    public Post(Long postId, String title, String content,
+                String name, String email, String profile_img,
+                String like, String category, LocalDateTime modifiedAt,
+                LocalDateTime createdAt) {
+        super();
+    }
+
+
+//    // 코멘트 클래스와 one-to-many 팹핑
+//    @JsonManagedReference
+//    @OneToMany(mappedBy = "post", orphanRemoval = true)
+//    private List<Comment> comments;
+
+    //게시글 수정
+    public void updatePost(PostResponseDto responseDto) {
+        this.title = responseDto.getTitle();
+        this.content = responseDto.getContent();
+        this.category = responseDto.getCategory();
+    }
+
+
+
+    //{
+    //”postId” :
+    //”title” :
+    //”content” :
+    //”name” :
+    //”email” :
+    //”profile_img” :
+    //”like”(좋아요):
+    //”category”(카테고리) :
+    //”modifiedAt” :
+    //”createdAt”:
+    //}
+
+
+
+
+
+
+
 }
