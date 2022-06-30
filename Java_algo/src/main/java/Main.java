d51256f938b0d3ae624c8da58dcaa3fe1bf6a3a5
kokomong2 <ko9603@naver.com>
2022-06-30 22:57:48 +0900

    추가 : 좋아요 기능, SSL기능(시도)

--- Java_algo/src/main/java/Main.java
+++ Java_algo/src/main/java/Main.java
@@ -1,37 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int start = a;
+        String stringA="" ;
+        String back = "";
+        int count = 0;
+        int sum = 0;
         while(true){
-            int count =0;
-            int n= sc.nextInt();
-            if(n==0)break;
-            boolean[] isPrime = new boolean[2*n+1];
-            Arrays.fill(isPrime , true);
 
-            // 소수는 true
-            // 0, 1은 소수가 아니므로 false
-            isPrime [0] = isPrime [1] = false;
-            for(int i=2; i*i<=2*n; i++){
-                // 만약 i가 소수 혹은 아직 지워지지 않았다면
-                if(isPrime[i]){
-                    // i의 배수 j들에 대해 isPrime[j] = false; 로 둔다.
-                    // i*i미만의 배수는 이미 지워졌으므로 신경쓰지 않는다.
-                    for(int j=i*i; j<=2*n; j+=i) {
-                        isPrime[j] = false;
-                    }
-                }
-            }
-            // 1 ~ 120 사이의 소수 출력
-            for(int i=n+1; i<=2*n ;i++){
-                if(isPrime[i]) count++;
-            }
-            System.out.println(count);
+            if(a<10){
+                stringA = "0"+a;
+            }else{stringA = Integer.toString(a);}
+            String[] array = stringA.split("");
+            back = array[1];
+            sum = Integer.parseInt(array[0]) + Integer.parseInt(array[1]);
+            String sumback = Integer.toString(sum%10);
+            a = Integer.parseInt(back+sumback);
+            count++;
+            if(start== a)break;
+
         }
+        System.out.println(count);
+
     }
 }
+
+
+
+
+
+// int 여러개 받을 떄
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//    int M = Integer.parseInt(st.nextToken());
+//    int N = Integer.parseInt(st.nextToken());
+
+//int 하나만 받을때
+//    int koko = Integer.parseInt(br.readLine());
---
+++ hack/src/main/java/com/project/hack/repository/PostLikesRepository.java
@@ -0,0 +1,13 @@
+package com.project.hack.repository;
+
+import com.project.hack.model.PostLikes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostLikesRepository extends JpaRepository<PostLikes,Long> {
+
+    Optional<PostLikes> findByUserIdAndPostId(Long userId, Long postId);
+    List<PostLikes> findByPostId(Long postId);
+}
